23786dbbab45873b47a6231a629c376515ce8c21
thắng <app443325@gmail.com>
2026-02-18 09:10:58 +0700

    index.js updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-
+import productRoute from './routes/productRoute.js';
 import config from './config.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/api', productRoute);
 
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl}`),
